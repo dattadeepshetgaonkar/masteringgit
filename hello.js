b221ca27eaf8775eafef8f8e014225351602faae
Dattadeep <shetgaonkardattadeep@gmail.com>
2026-07-18 15:18:15 +0530

    Revert "add revert console.log"
    This reverts commit 0670e5433666221d963326c79e348d15657b9a43.

--- hello.js
+++ hello.js
@@ -1,8 +1,12 @@
 console.log("Hello, World!");
 console.log("Hello, GITHUB!");
 console.log("Hello, this is from dev-adrian");
+<<<<<<< HEAD
 console.log("Hello, this is from dev");
 
 console.log("trying oout revert ");
 
 console.log("revert accepting changes");
+=======
+console.log("Hello, this is from dev");
+>>>>>>> parent of 0670e54 (add revert console.log)
