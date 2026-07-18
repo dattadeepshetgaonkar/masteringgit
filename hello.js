0670e5433666221d963326c79e348d15657b9a43
Dattadeep <shetgaonkardattadeep@gmail.com>
2026-07-18 15:12:45 +0530

    add revert console.log

--- hello.js
+++ hello.js
@@ -2,3 +2,5 @@ console.log("Hello, World!");
 console.log("Hello, GITHUB!");
 console.log("Hello, this is from dev-adrian");
 console.log("Hello, this is from dev");
+
+console.log("trying oout revert ");
